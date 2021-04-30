fb36e818b33f7b31343f785cfdda73064e731095
iuscript <6593942@qq.com>
2021-04-30 22:30:03 +0800

    add function

--- contracts/GameItem.sol
+++ contracts/GameItem.sol
@@ -3,20 +3,31 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract GameItem is ERC721, ERC721URIStorage, Ownable {
+contract GameItem is ERC721, ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
+    string  public constant version  = "1.1.0";
 
-    constructor() ERC721("GameItem", "ITM") {}
+    // --- Auth ---
+    mapping (address => uint) public wards;
+    function rely(address guy) external auth { wards[guy] = 1; }
+    function deny(address guy) external auth { wards[guy] = 0; }
+    modifier auth {
+        require(wards[msg.sender] == 1, "not-authorized");
+        _;
+    }
+
+    constructor() ERC721("GameItem", "ITM") {
+        wards[msg.sender] = 1;
+    }
     
     function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
         super._burn(tokenId);
     }
 
-    function awardItem(address player, string memory _tokenURI) external onlyOwner returns (uint256) {
+    function awardItem(address player, string memory _tokenURI) external auth returns (uint256) {
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
         _mint(player, newItemId);
@@ -24,7 +35,7 @@ contract GameItem is ERC721, ERC721URIStorage, Ownable {
         return newItemId;
     }
 
-    function setTokenURI(uint256 tokenID, string memory _tokenURI) external onlyOwner {
+    function setTokenURI(uint256 tokenID, string memory _tokenURI) external auth {
         _setTokenURI(tokenID, _tokenURI);
     }
     
--- contracts/NftMarket.sol
+++ contracts/NftMarket.sol
@@ -8,6 +8,7 @@ interface ERC20Like {
 
 interface ERC721Like {
     function transferFrom( address from, address to, uint256 tokenId) external;
+    function awardItem(address player, string memory _tokenURI) external returns (uint256);
 }
 
 contract Owned {
@@ -39,6 +40,11 @@ contract Owned {
 
 contract NftMarket is Owned {
     address public nftAsset;
+    address public abcToken;
+    string  public constant version  = "1.2.1";
+    address public revenueRecipient;
+    uint256 public mintFee;
+    uint256 public transferFee;
 
     struct Offer {
         bool isForSale;
@@ -48,6 +54,7 @@ contract NftMarket is Owned {
         uint256 minValue;
         uint256 endTime;
         address paymentToken;
+        uint256 reward;
     }
 
     struct Bid {
@@ -56,28 +63,69 @@ contract NftMarket is Owned {
         uint256 value;
     }
 
-    string  public constant version  = "1.1.0";
+    struct Royalty {
+        address originator;
+        uint256 royalty;
+        bool    recommended;
+    }
     
-    mapping(uint256 => Offer) public nftOfferedForSale;
-    mapping(uint256 => Bid) public nftBids;
-    mapping(address => uint256) public pendingWithdrawals;
+    mapping(uint256 => Offer)     public nftOfferedForSale;
+    mapping(uint256 => Bid)       public nftBids;
+    mapping(uint256 => Royalty)   public royalty;
+    mapping(address => uint256)   public pendingWithdrawals;
+    mapping(uint256 => address[]) public bidders;
+    mapping(uint256 => mapping(address => bool)) public bade;
 
     event Offered(uint indexed tokenID, uint256 minValue, address paymentToken);
     event BidEntered(uint256 indexed tokenID, address indexed fromAddress, uint256 value);
     event Bought(address indexed fromAddress, address indexed toAddress, uint256 indexed tokenID, uint256 value, address paymentToken);
     event NoLongerForSale(uint256 indexed tokenID);
     event Withdraw(address indexed who, uint256 value);
-    event AuctionPass(uint256 indexed tokenID, uint256 timestamp);
+    event AuctionPass(uint256 indexed tokenID);
 
-    constructor(address _nftAsset) {
-        nftAsset = _nftAsset;
+    constructor(address _nftAsset, address _abcToken, address _revenueRecipient, uint256 _mintFee, uint256 _transferFee) {
+        require(_transferFee < 20, "Excessive transaction fees");
+        nftAsset         = _nftAsset;
+        abcToken         = _abcToken;
+        revenueRecipient = _revenueRecipient;
+        mintFee          = _mintFee;
+        transferFee      = _transferFee;
     }
 
-    function sell(uint256 tokenID, bool isBid, uint256 minSalePrice, uint256 endTime, address paymentToken) external {
+    function NewNft(address player, string memory _tokenURI, uint256 _royalty) external returns (uint256) {
+        require(_royalty < 30, "Excessive copyright fees");
+        
+        ERC20Like(abcToken).transferFrom(msg.sender, address(this), mintFee);
+        ERC20Like(abcToken).transfer(revenueRecipient, mintFee);
+        
+        uint256 tokenID = ERC721Like(nftAsset).awardItem(player, _tokenURI);
+
+        royalty[tokenID] = Royalty(msg.sender, _royalty, false);
+
+        return tokenID;
+    }
+
+    function recommend(uint256 tokenID) external onlyOwner {
+        royalty[tokenID].recommended = true;
+    }
+
+    function cancelRecommend(uint256 tokenID) external onlyOwner {
+        royalty[tokenID].recommended = false;
+    }
+
+    /// @notice 挂单出售nft资产
+    /// @param tokenID tokenID
+    /// @param isBid 拍卖模式标记
+    /// @param minSalePrice 起拍价 或 一口价
+    /// @param endTime 拍卖模式，出价终止时间
+    /// @param paymentToken 用于支付代币的种类
+    /// @param reward 拍卖模式，用于奖励拍卖参与者的分成比率
+    function sell(uint256 tokenID, bool isBid, uint256 minSalePrice, uint256 endTime, address paymentToken, uint256 reward) external {
         require(endTime <= block.timestamp + 30 days);
         require(endTime > block.timestamp + 5 minutes);
+        require(reward < 100 - transferFee - royalty[tokenID].royalty, "Excessive reward");
         ERC721Like(nftAsset).transferFrom(msg.sender, address(this), tokenID);
-        nftOfferedForSale[tokenID] = Offer(true, tokenID, msg.sender, isBid, minSalePrice, endTime, paymentToken);
+        nftOfferedForSale[tokenID] = Offer(true, tokenID, msg.sender, isBid, minSalePrice, endTime, paymentToken, reward);
         emit Offered(tokenID, minSalePrice, paymentToken);
     }
 
@@ -97,12 +145,20 @@ contract NftMarket is Owned {
         require(offer.isForSale, "nft not actually for sale");
         require(!offer.isBid, "nft is auction mode");
 
+        uint256 share1 = offer.minValue * transferFee / 100; // 平台分润
+        uint256 share2 = offer.minValue * royalty[tokenID].royalty / 100; // 艺术家分润
+
         if (offer.paymentToken != address(0)) {
             ERC20Like(offer.paymentToken).transferFrom(msg.sender, address(this), offer.minValue);
-            ERC20Like(offer.paymentToken).transfer(offer.seller, offer.minValue);
+
+            ERC20Like(offer.paymentToken).transfer(revenueRecipient, share1);
+            ERC20Like(offer.paymentToken).transfer(royalty[tokenID].originator, share2);
+            ERC20Like(offer.paymentToken).transfer(offer.seller, offer.minValue - share1 - share2);
         } else {
             require(msg.value >= offer.minValue, "Sorry, your credit is running low");
-            pendingWithdrawals[offer.seller] += offer.minValue;
+            pendingWithdrawals[revenueRecipient] += share1;
+            pendingWithdrawals[royalty[tokenID].originator] += share2;
+            pendingWithdrawals[offer.seller] += offer.minValue - share1 -share2;
         }
         ERC721Like(nftAsset).transferFrom(address(this), msg.sender, tokenID);
         emit Bought(offer.seller, msg.sender, tokenID, offer.minValue, offer.paymentToken);
@@ -121,6 +177,11 @@ contract NftMarket is Owned {
         require(offer.isForSale, "nft not actually for sale");
         require(offer.isBid, "nft must beauction mode");
         require(block.timestamp < offer.endTime, "The auction is over");
+
+        if (!bade[tokenID][msg.sender]) {
+            bidders[tokenID].push(msg.sender);
+            bade[tokenID][msg.sender] == true;
+        }
 
         Bid memory bid = nftBids[tokenID];
         if (offer.paymentToken != address(0)) {
@@ -147,22 +208,46 @@ contract NftMarket is Owned {
     function deal(uint256 tokenID) external {
         Offer memory offer = nftOfferedForSale[tokenID];
         require(offer.isForSale, "nft not actually for sale");
-        require(offer.isBid, "nft must beauction mode");
+        require(offer.isBid, "must be auction mode");
         require(offer.endTime < block.timestamp, "The auction is not over yet");
 
         Bid memory bid = nftBids[tokenID];
 
         if (bid.value >= offer.minValue) { // bid.value > 0 即可，上面代码保证了出价必须高于 offer.minValue
-            if (offer.paymentToken == address(0)) {
-                pendingWithdrawals[offer.seller] += bid.value;
+            uint256 share1 = bid.value * transferFee / 100; // 平台分润
+            uint256 share2 = offer.minValue * royalty[tokenID].royalty / 100; // 艺术家分润
+            uint256 share3 = 0; // 拍卖参与者分润
+            uint256 tempC = bid.value * offer.reward / 100 / bidders[tokenID].length -1;
+
+            if (offer.paymentToken != address(0)) {
+                for (uint256 i=0; i < bidders[tokenID].length - 1; i++) {
+                    ERC20Like(offer.paymentToken).transfer(bidders[tokenID][i], tempC);
+                    share3 += tempC;
+                }
+                
+                ERC20Like(offer.paymentToken).transfer(revenueRecipient, share1);
+                ERC20Like(offer.paymentToken).transfer(royalty[tokenID].originator, share2);
+                ERC20Like(offer.paymentToken).transfer(offer.seller, bid.value - share1 - share2 - share3);
             } else {
-                ERC20Like(offer.paymentToken).transfer(offer.seller, bid.value);
+                for (uint256 i=0; i < bidders[tokenID].length - 1; i++) {
+                    pendingWithdrawals[bidders[tokenID][i]] += tempC;
+                    share3 += tempC;
+                }
+                
+                pendingWithdrawals[revenueRecipient] += share1;
+                pendingWithdrawals[royalty[tokenID].originator] += share2;
+                uint256 tempD = bid.value - share1 - share2 -share3;
+                pendingWithdrawals[offer.seller] += tempD;
             }
+
+            bade[tokenID][msg.sender] = false;
+            delete bidders[tokenID];
+
             ERC721Like(nftAsset).transferFrom(address(this), bid.bidder, tokenID);
             emit Bought(offer.seller, bid.bidder, tokenID, bid.value, offer.paymentToken);
         } else { // 没人出价，导致流拍
             ERC721Like(nftAsset).transferFrom(address(this), offer.seller, tokenID);
-            emit AuctionPass(tokenID, block.timestamp);
+            emit AuctionPass(tokenID);
         }
         delete nftOfferedForSale[tokenID];
         delete nftBids[tokenID];
